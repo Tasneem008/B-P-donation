e0180c8ab85e809e0c96890432a70d4742dbeb7c
ayeshamethila <ayeshamethila@gmail.com>
2025-08-13 20:30:53 +0600

    blood donation model

---
+++ models/BloodDonation.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const bloodDonationSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+  nid: { type: String, required: true },
+  bloodgroup: { type: String, required: true },
+  donorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  lastDonation: { type: Date, default: Date.now },
+  status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("BloodDonation", bloodDonationSchema, "blooddonations");
