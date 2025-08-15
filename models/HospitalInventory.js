e9e26f215a3925c103c7ccd922cec73adb970059
Tasneem008 <tasneemritee008@gmail.com>
2025-08-16 00:39:50 +0600

    created a newschema

---
+++ models/HospitalInventory.js
@@ -0,0 +1,20 @@
+// models/HospitalInventory.js
+const mongoose = require("mongoose");
+
+const InventorySchema = new mongoose.Schema(
+  {
+    hospital: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    kind: { type: String, enum: ["blood", "plasma"], required: true },
+    bloodType: {
+      type: String,
+      enum: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+      required: true,
+    },
+    units: { type: Number, min: 0, default: 0 },
+  },
+  { timestamps: true }
+);
+
+
+
+module.exports = mongoose.model("HospitalInventory", InventorySchema);
