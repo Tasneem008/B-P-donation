931d4e18137e12da0e882b9f66d52ba61e6c1a72
Tasneem008 <tasneemritee008@gmail.com>
2025-08-22 14:23:09 +0600

    added new variable

--- controllers/hospital.controller.js
+++ controllers/hospital.controller.js
@@ -1,26 +1,36 @@
-const BloodRequest = require("../models/BloodRequest");
+const BloodDonation = require("../models/BloodDonation.js");
+const BloodRequest = require("../models/BloodRequest.js");
 
 exports.getDashboard = async (req, res) => {
   try {
     const totalUnits = 120; // Placeholder, can be calculated if you have stock collection
-    const donorPending = 14; // Example static value, you can make it dynamic
-    const recipientPending = await BloodRequest.countDocuments({
-      status: "Pending",
-    });
-    const recipientFulfilled = await BloodRequest.countDocuments({
-      status: "Fulfilled",
+    const donorPending =
+      (await BloodDonation.countDocuments({
+        status: "pending",
+      })) || 0;
+
+    const donorApproved =
+      (await BloodDonation.countDocuments({
+        status: "approved",
+      })) || 0;
+
+    const recipientPending =
+      (await BloodRequest.countDocuments({
+        status: "pending",
+      })) || 0;
+    const recipientAccepted = await BloodRequest.countDocuments({
+      status: "accepted",
     });
 
     // Fetch all requests for the table
     const requests = await BloodRequest.find().sort({ createdAt: -1 });
 
     res.render("hospital-panel", {
-      data: {
-        availableUnits: totalUnits,
-        donorPending,
-        recipientPending,
-        recipientFulfilled,
-      },
+      availableUnits: totalUnits,
+      donorPending,
+      donorApproved,
+      recipientPending,
+      recipientAccepted,
       requests,
     });
   } catch (err) {
@@ -72,7 +82,13 @@ exports.rejectRequest = async (req, res) => {
   }
 };
 
-
-exports.getDonorRequestsPage = (req, res)=>{
-    res.render('donor-requests')
-}
+exports.getDonorRequestsPage = async (req, res) => {
+  try {
+    const bloodDonorRequest = await BloodDonation.find({}).populate("donorId");
+    console.log(bloodDonorRequest);
+    res.render("donor-requests", { bloodDonorRequest });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+};
