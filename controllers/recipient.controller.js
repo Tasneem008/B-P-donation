62ae803b9a9db72a6e45ba6c9e98dac0a482e758
Tasneem008 <tasneemritee008@gmail.com>
2025-08-23 17:42:56 +0600

    might not need this later

--- controllers/recipient.controller.js
+++ controllers/recipient.controller.js
@@ -1,13 +1,5 @@
 const User = require("../models/User.js");
 
-exports.showRecipientDashboard = async (req, res) => {
-  const user = await User.findById(req.session.userId);
-  res.render("recipient-dashboard", { user });
-};
-
-exports.showRecipientHistory = (req, res) => {
-  res.render("recipient-history");
-};
 
 // exports.submitRequest = async (req, res) => {
 //   const { bloodType, hospital, location, description, requestedDate } = req.body;
